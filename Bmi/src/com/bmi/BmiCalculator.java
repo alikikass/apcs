6a5a950735f34d4cbb2ec1d6dd57c9a63206a986
Aliki Kassioti <alikikassioti@alikis-mbp.lan>
2020-03-26 19:17:23 -0500

    final project

--- Bmi/src/com/bmi/BmiCalculator.java
+++ Bmi/src/com/bmi/BmiCalculator.java
@@ -6,20 +6,26 @@ public class BmiCalculator {
     public static void main(String[] args) {
         //create new scanner
         Scanner input = new Scanner (System.in);
-        //ask user for height and weight
-        System.out.println("Enter your height in inches: ");
-        int height = input.nextInt();
-        System.out.println("Enter your weight in pounds: ");
-        int weight = input.nextInt();
-        //make BMI a variable
-        int bmi = (weight * 703) / (height * height);
-        //calculate BMI
-        System.out.println("Your BMI is " + bmi );
-        //print out information from Department of Health and Human Services
-        System.out.println("\nBMI VALUES:" + "\nUnderweight: less than 18.5" + "\nNormal: between 18.5 and 24.9"
-                + "\nOverweight: between 25 and 29.9" + "\nObese: 30 or greater");
-        //tell user what their BMI is
 
+        System.out.println("Enter '0' to start and '-1' to quit: ");
+        int i = input.nextInt();
+        while(i != -1) {
+            //ask user for height and weight
+            System.out.println("Enter your height in inches: ");
+            int height = input.nextInt();
+            System.out.println("Enter your weight in pounds: ");
+            int weight = input.nextInt();
+            //make BMI a variable
+            int bmi = (weight * 703) / (height * height);
+            //calculate BMI
+            System.out.println("Your BMI is " + bmi);
+            //print out information from Department of Health and Human Services
+            System.out.println("\nBMI VALUES:" + "\nUnderweight: less than 18.5" + "\nNormal: between 18.5 and 24.9"
+                    + "\nOverweight: between 25 and 29.9" + "\nObese: 30 or greater" + "\n ");
+
+            System.out.println("Enter '0' to start and '-1' to quit: ");
+             i = input.nextInt();
+            }
 
     }
 }
--- Multiples/src/com/multiples/Multiples.java
+++ Multiples/src/com/multiples/Multiples.java
@@ -6,18 +6,24 @@ public class Multiples {
     public static void main(String[] args) {
         //create a new scanner
         Scanner input = new Scanner (System.in);
-        //ask user for numbers
-        System.out.println("Enter your first number:  ");
-        int num1 = input.nextInt();
-        System.out.println("Enter your second number: ");
-        int num2 = input.nextInt();
-        //determine if number 1 is a multiple of number 2 and tell the user
-       if ((num1 % num2) == 0)
-           System.out.println( num1 + " is a multiple of " + num2 );
-       else
-           System.out.println( num1 + " is not a multiple of " + num2);
 
+        System.out.println("Enter '0' to start and '-1' to quit: ");
+        int i = input.nextInt();
+        while(i != -1) {
+            //ask user for numbers
+            System.out.println("Enter your first number :  ");
+            int num1 = input.nextInt();
+            System.out.println("Enter your second number: ");
+            int num2 = input.nextInt();
+            //determine if number 1 is a multiple of number 2 and tell the user
+            if ((num1 % num2) == 0)
+                System.out.println(num1 + " is a multiple of " + num2);
+            else
+                System.out.println(num1 + " is not a multiple of " + num2);
 
+            System.out.println("Enter '0' to start and '-1' to quit: ");
+             i = input.nextInt();
+             }
 
 
 
